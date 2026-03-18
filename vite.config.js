b9b985cbac361ba677a714a9f9ee21436f9f25fa
Lan Yang <lan980719@gmail.com>
2026-03-18 16:55:59 +0100

    to trigger rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lanyang-2d/",
+  base: "/",  // 固定/ for Workers
   plugins: [react()],
 });
