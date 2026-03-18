9880c16bbffba89168d221fdcc8e1460f8daf02c
Lan Yang <lan980719@gmail.com>
2026-03-18 16:54:01 +0100

    to trigger rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.DEPLOY_TARGET === "cloudflare" ? "/" : "/lanyang-2d/",
+  base: "/lanyang-2d/",
   plugins: [react()],
 });
